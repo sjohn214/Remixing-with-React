655f33a4d80189057ffd758fa6c01bdaac0661fd
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-03-03 09:53:46 -0500

    Add react, components, install npm packages

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Searchbar from "./Searchbar"
+
+
+function Navbar(){
+    return (
+        <nav className="navbar"><Searchbar/></nav>
+    )
+}
+export default Navbar;
